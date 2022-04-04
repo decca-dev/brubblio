d57d7551a1bd63486f662b90d5f0bdb1e951019d
decca-dev <medcherbani@gmail.com>
2022-04-04 22:46:32 +0000

    more context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+import { useEffect, useState } from "react";
+import UserContext from "../components/contexts/UserContext";
+interface User {
+  username: string;
+  avatar: string;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    const name =
+      localStorage.getItem("username") ||
+      uniqueNamesGenerator({
+        dictionaries: [adjectives, animals, colors],
+        length: 2,
+        separator: " ",
+      });
+    const userAvatar = localStorage.getItem("avatar") || "1";
+    setUser({ username: name, avatar: userAvatar });
+  }, []);
+
+  return (
+    <UserContext.Provider value={user!}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
+}
+
+export default MyApp;
